c17c9650e42cd60dc0dc258b7f219a51e84ee9e4
Brayan Ellis <bryanellis.dev@gmail.com>
2024-10-09 23:12:16 -0400

    feat: creando archivo para practicar con pack

---
+++ posicionamiento_diseno_pack.py
@@ -0,0 +1,19 @@
+# POSICIONAMIENTO Y DISEÑO
+# PACK
+from tkinter import Tk, Label
+
+root = Tk() # se crea la instancia de la clase Tk, que representa la ventana principal
+
+root.title("Curso de Tkinter") # agregando titulo
+
+# VENTANA
+root.geometry("800x600") # tamaño de la ventana
+root.resizable(True, True) # permite que el usuario cambie el tamaño de la ventana ancho y alto
+root.minsize(200, 200) # tamaño mínimo
+root.maxsize(960, 720) # tamaño máximo
+
+
+etiqueta = Label(text="\n !Hola Mundo¡ ") # se crea la etiqueta 'widget' con su texto
+etiqueta.pack() # esta sentencia sitúa la etiqueta en la ventana
+
+root.mainloop() # loop infinito que inicia el programa
