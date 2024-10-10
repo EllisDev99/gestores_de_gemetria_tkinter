6bba04656cfcedba1e2c4efbb062b961cdde806b
Brayan Ellis <bryanellis.dev@gmail.com>
2024-10-09 23:08:08 -0400

    feat: estableciendo los tamaños mínimos y máximos de la ventana y agregando el archivo elmininado

--- primera_ventana.py
+++ primera_ventana.py
@@ -4,7 +4,12 @@ root = Tk() # se crea la instancia de la clase Tk, que representa la ventana pri
 
 root.title("Curso de Tkinter") # agregando titulo
 
-root.geometry("200x200") # tamaño de la ventana
+# VENTANA
+root.geometry("800x600") # tamaño de la ventana
+root.resizable(True, True) # permite que el usuario cambie el tamaño de la ventana ancho y alto
+root.minsize(200, 200) # tamaño mínimo
+root.maxsize(960, 720) # tamaño máximo
+
 
 etiqueta = Label(text="\n !Hola Mundo¡ ") # se crea la etiqueta 'widget' con su texto
 etiqueta.pack() # esta sentencia sitúa la etiqueta en la ventana
