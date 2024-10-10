edfe9ec8161721b40beb91fd4f6c4f9ab3f67f4f
Brayan Ellis <bryanellis.dev@gmail.com>
2024-10-09 22:49:10 -0400

    feat: agregando título, tamaño de la ventana y cambiando nombre del archivo

--- primera_ventana.py
+++ primera_ventana.py
@@ -2,7 +2,9 @@ from tkinter import Tk, Label
 
 root = Tk() # se crea la instancia de la clase Tk, que representa la ventana principal
 
-root.title("Curso de Tkinter")
+root.title("Curso de Tkinter") # agregando titulo
+
+root.geometry("200x200") # tamaño de la ventana
 
 etiqueta = Label(text="\n !Hola Mundo¡ ") # se crea la etiqueta 'widget' con su texto
 etiqueta.pack() # esta sentencia sitúa la etiqueta en la ventana
